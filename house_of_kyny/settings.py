f90cfcc13537a7adb301626cadcee416825e0065
abbytamp <abbyst14@gmail.com>
2024-09-16 00:22:02 +0700

    push ulang ke github dan pws

--- house_of_kyny/settings.py
+++ house_of_kyny/settings.py
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","https://pbp.cs.ui.ac.id/web/project/abby.shelley/hokyny", "https://pbp.cs.ui.ac.id/web/project/abby.shelley/hokyny"]
