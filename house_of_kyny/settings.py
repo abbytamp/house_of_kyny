4b4246ab1503b61cd15f6087460fb1c38aaf1ddd
abbytamp <abbyst14@gmail.com>
2024-09-16 00:25:31 +0700

    push ulang

--- house_of_kyny/settings.py
+++ house_of_kyny/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","https://pbp.cs.ui.ac.id/web/project/abby.shelley/hokyny", "https://pbp.cs.ui.ac.id/web/project/abby.shelley/hokyny"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://pbp.cs.ui.ac.id/web/project/abby.shelley/hokyny", "https://pbp.cs.ui.ac.id/web/project/abby.shelley/hokyny"]
