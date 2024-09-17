76deea78acbe97b956c670a6d63defc6f0bda48e
abbytamp <abbyst14@gmail.com>
2024-09-17 16:41:02 +0700

    Membuat input form, menambahkan 4 fungsi views, serta membuat routing URL

--- main/models.py
+++ main/models.py
@@ -1,7 +1,9 @@
-# Create your models here.
+import uuid
+
 from django.db import models
 
-class MoodEntry(models.Model):
+class ProductEntry(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) 
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
